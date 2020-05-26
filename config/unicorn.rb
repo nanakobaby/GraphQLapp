2926cb69b4eab0134e33aa95a39907243d22d24a
nanako kan <n.k.0213.0529@gmail.com>
2020-05-26 13:56:41 +0900

    nginx設定ファイルを編集

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 preload_app true
 timeout 5
 worker_processes 4
-listen '/tmp/nginx.socket', backlog: 1024
+listen '/Users/kannanako/DIC/nginxapp/tmp/unicorn.sock', backlog: 1024
 
 before_fork do |server,worker|
 	FileUtils.touch('/tmp/app-initialized')
