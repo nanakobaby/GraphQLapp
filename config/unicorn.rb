c97a63f9b8b1fabba0f82c0444ec8db07ac4fc20
nanako kan <n.k.0213.0529@gmail.com>
2020-05-26 09:05:15 +0900

    NGINXビルドパックの procfileを更新する

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,23 @@
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
-worker_processes 4
-listen 'unix:///tmp/nginx.socket', backlog: 1024
 
-before_fork do |server,worker|
-    FileUtils.touch('/tmp/app-initialized')
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end 
+
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
